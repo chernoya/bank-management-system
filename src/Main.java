3a9d205f6cd7ab523c33c1891591c455f7bfe586
chernoya <adilrabbani005@gmail.com>
2023-12-15 16:01:36 +0500

    stage 0, outline specified on what to do

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+            a) trivia game with classes and objects
+
+            b) Bank management system:
+            create a bank management system which has the following components:
+            person (which saves first, middle and last name, age and a "social security number")
+            bank (which saves a list of customers and their checking accounts, maximum overdraw amount)
+            checking account (should have a current value, methods for taking and depositing money)
+            opening a checking account should only work via a bank and must require a certain amount of initial deposit
+
+            BONUS: make the banking system interactive
+
+        */
+
+    }
+}
